4982863c9474cc0773132ca74bcc854a683d6424
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-01-24 22:09:39 -0500

    Probando el puerto libre

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo');
+const express = require('express');
+const app = express();
+const port = 0;
+// req para coger las respuesta los usuarios
+// res para mostrar en pantalla
+app.get("/", (req, res) => {
+  res.send('hola mundo') //Enviamos algo
+})
+
+app.listen(port, function () {
+  console.log('Mi puerto es ', this.address().port)
+});
